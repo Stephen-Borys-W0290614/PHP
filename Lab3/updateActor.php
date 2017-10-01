e4cecfd26aa6ef868ef0610fc88c33c5f60ae313
inet2005 <w0290614@nscc.ca>
2017-10-01 13:54:38 -0300

    Work on Lab 3 9/30

--- Lab3/updateActor.php
+++ Lab3/updateActor.php
@@ -1,9 +1,9 @@
-<?php
+<?php/*
 require("dbconn.php");
 require("updateActor1.php");
-$first = ($_POST['first']);
-$last = ($_POST['last']);
-$id = getID();
+$first = ($_POST['firstname']);
+$last = ($_POST['lastname']);
+
 
 echo $id;
 
@@ -15,6 +15,32 @@ if ($conn->connect_error) {
 }
 
 $sql = "UPDATE actor SET first_name = $first, last_name= $last WHERE id=$id;";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . $conn->error;
+}
+
+$conn->close();
+*/?>
+
+<?php
+require("dbconn.php");
+$first = $_POST['fName'];
+$last = $_POST['lName'];
+$id1 = $_POST['id1'];
+
+echo $id1;
+
+// Create connection
+$conn = connectToDatabase();
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "UPDATE actor SET first_name = $first, last_name= $last WHERE id='$id1'";
 
 if ($conn->query($sql) === TRUE) {
     echo "Record updated successfully";
--- Lab3/updateActor1.php
+++ Lab3/updateActor1.php
@@ -12,19 +12,40 @@
 </head>
 <body>
 <?php
-$id = getID();
-echo $id;
 ?>
 
+<?php
+require("dbconn.php");
+$conn = connectToDatabase();
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+mysqli_select_db("my_db", $conn);
+$id = ($_POST['id']);
+
+$result = mysqli_query($conn,"SELECT * FROM actor WHERE actor_id = '$id'")->fetch_array();
+
+
+
+
+
+?>
+
+
 <form action="updateActor.php" method="post">
-    <p>Enter First Name Update: <input type="text" name="first" /></p>
-    <p>Enter Last Name Update: <input type="text" name="last" /></p>
+    <p> Update First Name: <input name="fName" value="<?php echo $result['first_name']?>" <br/></p>
+    <p> Update Last Name: <input name="lName" value="<?php echo $result['last_name']?>" <br/></p>
+    <p> ID: <input type="number" name="id1" value="<?php echo $id ?>">
     <p><input type="submit" name="Submit" value="Send Form" /></p>
 </form>
 
 
+
 </body>
 
-
+<?php
+mysqli_close($conn);
+?>
 
 </html>
